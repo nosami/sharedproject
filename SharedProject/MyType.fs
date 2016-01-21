b7844b13b6ee1d81deebeb5ba0af40707d82f8c8
nosami <jasonimison@gmail.com>
2016-01-21 11:06:23 +0000

    test

---
+++ SharedProject/MyType.fs
@@ -0,0 +1,7 @@
+﻿namespace SharedProject
+module mymodule =
+  let thing = "F#"
+
+type Class1() = 
+    member this.X = "F#"
+
---
+++ iosapp/AppDelegate.fs
@@ -0,0 +1,16 @@
+﻿namespace iosapp
+
+open System
+open SharedProject
+open UIKit
+open Foundation
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+    let x = mymodule.thing
+    override val Window = null with get, set
+
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching (app, options) =
+        true
